683a649957a382ef7fba969a2d0cd9bf3b38a0c6
wqsz7xn <wqsz7xn@protonmail.com>
2021-12-06 14:30:36 +1300

    add tutorials-moonriver-evm-starter.hbs

--- templates.ts
+++ templates.ts
@@ -1,4 +1,5 @@
 // basename file, template description
 export let templates: Record<string, string> = {
   'subql-starter': 'Default starter project for subql',
+  'tutorials-moonriver-evm-starter': 'Moonbeam evm tutorial',
 };
